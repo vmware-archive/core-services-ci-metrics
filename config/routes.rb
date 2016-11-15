1fa94805feeb18ed92630bbbb83a07e165646fc4
Difan Zhao <dzhao@pivotal.io>
2016-11-15 14:37:16 -0800

    Add metrics page
    [#134201257]
    
    Signed-off-by: Jeff Schnitzer <jschnitzer@pivotal.io>

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :controller => 'home', :action => 'index'
 
+  get '/metrics', to: 'metrics#index'
+
   namespace :api do
     namespace :v1 do
       resources :failures, only: [:create, :update, :index, :show]
+      resources :metrics, only: [:index]
     end
   end
 end
